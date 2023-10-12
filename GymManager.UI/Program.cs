ca075147cff48819bc70ee44a0d0d76d81edb7d4
chrisonoo <chris@onoo.no>
2023-10-12 07:59:07 +0200

    docs: move documentation to _docs

--- GymManager.UI/Program.cs
+++ GymManager.UI/Program.cs
@@ -6,18 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure();
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 app.UseInfrastructure();
 
-// Configure the HTTP request pipeline.
 if(!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
