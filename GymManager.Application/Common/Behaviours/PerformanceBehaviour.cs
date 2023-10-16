23f7df11757808b2084ae78f6c9327cbd85521c0
chrisonoo <chris@onoo.no>
2023-10-16 15:23:03 +0200

    feat: configure middleware to log performance behaviours 3-15

---
+++ GymManager.Application/Common/Behaviours/PerformanceBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+using MediatR;
+
+using Microsoft.Extensions.Logging;
+
+namespace GymManager.Application.Common.Behaviours;
+
+public class PerformanceBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    private readonly ILogger _logger;
+    private readonly Stopwatch _timer;
+
+    public PerformanceBehaviour(ILogger<TRequest> logger)
+    {
+        _timer = new Stopwatch();
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    {
+        _timer.Start();
+
+        var response = await next();
+
+        _timer.Stop();
+
+        var elapsedMilliseconds = _timer.ElapsedMilliseconds;
+
+        if(elapsedMilliseconds > 500)
+        {
+            _logger.LogInformation("***** GymManager Long Running Request: {@Name} ({@ElapsedMilliseconds} milliseconds) {@Request} *****", typeof(TRequest).Name, elapsedMilliseconds, request);
+        }
+
+        return response;
+    }
+}
--- GymManager.Application/DependencyInjection.cs
+++ GymManager.Application/DependencyInjection.cs
@@ -14,6 +14,7 @@ public static class DependencyInjection
         services.AddMediatR(Assembly.GetExecutingAssembly());
 
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehaviour<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
 
         return services;
     }
