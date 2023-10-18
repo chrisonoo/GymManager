58886668ea3079c230c37ddf1eb51f7334722447
chrisonoo <chris@onoo.no>
2023-10-18 06:16:52 +0200

    feat create contact model 4-6

---
+++ GymManager.Application/Common/Interfaces/IEmail.cs
@@ -0,0 +1,7 @@
+﻿
+
+namespace GymManager.Application.Common.Interfaces;
+public interface IEmail
+{
+    Task SendAsync(string subject, string body, string to, string attachmentPath = null);
+}
---
+++ GymManager.Application/Contacts/Commands/SendContactEmail/SendContactEmailCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using GymManager.Application.Common.Interfaces;
+
+using MediatR;
+
+namespace GymManager.Application.Contacts.Commands.SendContactEmail;
+
+public class SendContactEmailCommandHandler : IRequestHandler<SendContactEmailCommand>
+{
+    private readonly IEmail _email;
+
+    public SendContactEmailCommandHandler(IEmail email)
+    {
+        _email = email;
+    }
+
+    public async Task<Unit> Handle(SendContactEmailCommand request, CancellationToken cancellationToken)
+    {
+        var body = $"Nazwa: {request.Name}.<br /></br >E-mail nadawcy: {request.Email}.<br /><br />Tytuł wiadomości: {request.Title}.<br /><br />Wiadomość: {request.Message}.<br /><br />Wysłano z: GymManager.";
+
+        await _email.SendAsync(
+           $"Wiadomość z GymManager: {request.Title}",
+           body,
+          "chris@onoo.no");
+
+        return Unit.Value;
+    }
+}
