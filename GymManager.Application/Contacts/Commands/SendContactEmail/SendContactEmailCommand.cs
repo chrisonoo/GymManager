03fc4b02be85c6716e4caa11b58058da00f8c530
Chris <chris@onoo.no>
2023-10-23 05:26:42 +0200

    feat: configure validation using DataAnnotation 4-12

--- GymManager.Application/Contacts/Commands/SendContactEmail/SendContactEmailCommand.cs
+++ GymManager.Application/Contacts/Commands/SendContactEmail/SendContactEmailCommand.cs
@@ -1,13 +1,22 @@
-﻿using MediatR;
+﻿using System.ComponentModel.DataAnnotations;
+
+using MediatR;
 
 namespace GymManager.Application.Contacts.Commands.SendContactEmail;
 public class SendContactEmailCommand : IRequest
 {
+    [Required(ErrorMessage = "Pole 'Imię i Nazwisko' jest wymagane.")]
     public string Name { get; set; }
 
+    [Required(ErrorMessage = "Pole 'Adres e-mail' jest wymagane.")]
+    [EmailAddress(ErrorMessage = "Pole 'Adres e-mail' nie jest prawidłowym adresem e-mail")]
     public string Email { get; set; }
 
+    [Required(ErrorMessage = "Pole 'Tytuł wiadomości' jest wymagane.")]
     public string Title { get; set; }
 
+    [Required(ErrorMessage = "Pole 'Wiadomość' jest wymagane.")]
     public string Message { get; set; }
+
+    public string AntySpamResult { get; set; }
 }
--- GymManager.Application/Contacts/Commands/SendContactEmail/SendContactEmailCommandValidator.cs
+++ GymManager.Application/Contacts/Commands/SendContactEmail/SendContactEmailCommandValidator.cs
@@ -4,9 +4,9 @@
 namespace GymManager.Application.Contacts.Commands.SendContactEmail;
 public class SendContactEmailCommandValidator : AbstractValidator<SendContactEmailCommand>
 {
-    public SendContactEmailCommandValidator()
-    {
-        RuleFor(x => x.Name)
-            .NotEmpty().WithMessage("Pole 'Imię i Nazwisko' jest wymagane.");
-    }
+    //public SendContactEmailCommandValidator()
+    //{
+    //    RuleFor(x => x.Name)
+    //        .NotEmpty().WithMessage("Pole 'Imię i Nazwisko' jest wymagane.");
+    //}
 }
